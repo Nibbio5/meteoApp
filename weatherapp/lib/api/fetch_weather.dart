8b5df9d501959f6260f38c0556e538112aa9c60c
Nibbio5 <71633363+Nibbio5@users.noreply.github.com>
2023-02-20 23:04:42 +0100

    final commit

--- weatherapp/lib/api/fetch_weather.dart
+++ weatherapp/lib/api/fetch_weather.dart
@@ -70,7 +70,7 @@ String apiURL(var lat, var lon) {
   print(tz);
   String url;
   url =
-      "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,snow_depth,weathercode,pressure_msl,surface_pressure,cloudcover,visibility,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&timezone=$tz&start_date=$sartTime&end_date=$endTime";
+      "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&hourly=temperature_2m,relativehumidity_2m,weathercode,cloudcover&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=$tz&start_date=$sartTime&end_date=$endTime";
   //print(url);
   return url;
 }
